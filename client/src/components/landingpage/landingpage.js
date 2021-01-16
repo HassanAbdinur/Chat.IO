f230dbafbfc0bdcaff02713143b0eacfd103fe3e
Hassan Abdinur <hassan.abdinur123@gmail.com>
2021-01-15 17:59:18 -0800

    added new file

---
+++ client/src/components/landingpage/landingpage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function landingpage() {
+    return(
+        <div className="landingpage">
+            <h1>you're successfully logged in</h1>
+        </div>
+    );  
+}
+
+export default landingpage;
